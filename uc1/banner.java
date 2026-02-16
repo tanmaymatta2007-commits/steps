a735860281653a01b85e14cd9932aca0293f0d24
tanmaymatta2007-commits <tanmaymatta2007@gmail.com>
2026-02-16 23:34:59 +0530

    Add banner class to display a pattern

---
+++ uc1/banner.java
@@ -0,0 +1,13 @@
+class banner
+{
+    public static void main(String args[])
+    {
+System.out.println("*** *** *** ***");
+System.out.println("* * * * * * *  ");
+System.out.println("* * * * * * *  ");
+System.out.println("* * * * *** ***");
+System.out.println("* * * * *     *");
+System.out.println("* * * * *     *");
+System.out.println("*** *** *   ***");
+    }
+}
