ad6845397962ec0c7fcc620a238879e67d244906
tanmaymatta2007-commits <tanmaymatta2007@gmail.com>
2026-02-22 22:56:37 +0530

    Refactor banner printing using String array

--- uc1/OOPSBannerApp.java
+++ uc1/OOPSBannerApp.java
@@ -1,11 +1,21 @@
-public class Main {
+public class uc4banner {
     public static void main(String[] args) {
-        System.out.println(String.join("   ***    ","      ***      ","   ******    "));
-        System.out.println(String.join(" **   **  ","    **   **    ","   **   **   "));
-        System.out.println(String.join(" **   **  ","    **   **    ","   **    **  "));
-        System.out.println(String.join(" **   **  ","    **   **    ","   **  **    "));
-        System.out.println(String.join(" **   **  ","    **   **    ","   **        "));
-        System.out.println(String.join(" **   **  ","    **   **    ","   **        "));
-        System.out.println(String.join("   ***    ","      ***      ","   **        "));
+
+        // Define a String array with size equal to number of lines in the banner
+        String[] lines = new String[7];
+
+        // Populate each index of the array with the corresponding banner line using String.join()
+        lines[0] = String.join("   ", "   ***  ", "   ***  ",   "  *****  ", "  ******");
+        lines[1] = String.join("   ", " **   **", " **   **",   "  **   **", " **    ");
+        lines[2] = String.join("   ", "**     **", "**     **", "**    **", "**     ");
+        lines[3] = String.join("   ", "**     **", "**     **", "*****   ", " ***** ");
+        lines[4] = String.join("   ", "**     **", "**     **", "**      ", "      **");
+        lines[5] = String.join("   ", " **   ** ", " **   ** ", "**      ", " **   **");
+        lines[6] = String.join("   ", "  ***    ", "  ***    ", "**      ", "  ***  ");
+
+        // Use a for-each loop to iterate through the array and print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
